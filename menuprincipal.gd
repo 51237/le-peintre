16bce434f20d59b2e9650e335e4e7878256cf793
bil333 <cbilel755@gmail.com>
2026-04-21 11:39:51 +0200

    menu_principal

---
+++ menuprincipal.gd
@@ -0,0 +1,9 @@
+extends Control
+@onready var first_level = preload("res://scenes/game.tscn")
+
+func _on_startbtn_button_down() -> void:
+	get_tree().change_scene_to_packed(first_level)
+
+
+func _on_quitbtn_button_down() -> void:
+	get_tree().quit()
