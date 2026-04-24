c979cc96c391f2c4cf588a5884cd7d753300fb1a
Azzer28 <azzer2828@gmail.com>
2026-04-24 12:24:37 +0200

    final

--- scripts/victory.gd
+++ scripts/victory.gd
@@ -3,4 +3,4 @@ extends Control
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_SPACE:
-			get_tree().change_scene_to_file("res://scenes/menuprincipal.tscn")
+			get_tree().change_scene_to_file("res://scenes/game.tscn")
